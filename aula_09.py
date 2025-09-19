25ff9820e04eb7c519f6271cb9bcfb10027f4eb2
Hellen Vieira <hellen.vieira@ee.ufcg.edu.br>
2025-09-19 15:34:45 -0300

    exemplo2

---
+++ aula_09.py
@@ -0,0 +1,19 @@
+Class Cliente: 
+    def __init__(self, nome, cpf)
+    self.nome = nome
+    self.cpf = cpf
+    
+Class Conta_bancaria:
+    def __init__(self, numero_conta, clinete_associado)
+    self.numero_conta = numero_conta
+    self.clinete_associado = clinete_associado
+
+    def mostrar_cliente_associado():
+        return clinete_associado
+    
+    def mostrar_numero_conta():
+        return numero_conta
+
+cliente01 = Conta_bancaria(123546, "Maria")
+
+mostrar_cliente_associado(cliente01)
